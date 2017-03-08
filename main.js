a9964f3c33de09301d0003bfdda8b76ba997cb49
KellieEll <ms.kellieell@gmail.com>
2017-03-08 00:14:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+			$("#compute").on("click", function (e) {
+
+						e.preventDefault();
+
+
+
+						$(.
+							"add") each(function () {
+
+								var x = $(this)
+								quantity = parseInt(x.val()),
+									compute = parseFloat(add.data("parseInt"))
+								var y = $(this)
+								quantity = parseInt(y.val()),
+									addition(x, y);
+							}
+							show_results();
+						}
+
+
+					}
